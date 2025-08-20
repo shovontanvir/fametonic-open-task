ef63847ae4457f5978b0e99e00058d9236f25af8
shovontanvir <shovon.tanvir.91@gmail.com>
2025-08-20 23:47:05 +0600

    :up:Update: Responsive check and fix

--- src/app/components/BannerList.jsx
+++ src/app/components/BannerList.jsx
@@ -2,10 +2,10 @@ import { listItems } from "../utils/constants";
 
 const BannerList = () => {
   return (
-    <ul className="lg:pr-20 my-3 md:my-5 text-center lg:text-start">
+    <ul className="lg:pr-20 my-3 md:my-5 text-start md:text-center lg:text-start">
       {listItems.map((item) => (
         <li
-          className="font-semibold my-3 leading-[1.25rem] text-base inline-flex"
+          className="font-semibold my-2 md:my-3 leading-[1.375rem] text-base inline-flex items-center"
           key={item.id}
         >
           <div className="mr-3">✨</div> <h1>{item.text}</h1>
